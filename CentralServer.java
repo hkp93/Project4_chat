b63bdb65ee0efe35129928d6ad4ac7f2f3e68e87
jankipatel07 <janipatel07@gmail.com>
2016-04-07 00:41:47 -0500

    client connect, need to check if server get msg or not

--- CentralServer.java
+++ CentralServer.java
@@ -3,7 +3,7 @@
  * PATNERS : HENVY PATEL & JANKI PATEL
  * DESCRIPTION: CentralServer.java : This file Establishes the central
  * 				connection between the clients by streaming
- * 			    and sockets for the program as well  as the basic GUI for 
+ * 			    and sockets for the program as well  as the basic GUI for
  *              running this application is established within
  *              this class.
  ********************************************************************************/
@@ -25,179 +25,234 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
- 
- 
+
+
 public class CentralServer implements ActionListener {
-	
-		// Here we will add all the required components of the chat application
-        private JFrame frame = new JFrame("Project 4 - Chat Application");
-        private JTextArea textArea = new JTextArea();
-        private JLabel online = new JLabel("Currently Online");
-        private JTextArea clientDisplay = new JTextArea();
-        private JTextField textField = new JTextField(30);
-        private JButton sendButton = new JButton("Send");
-        // to send the stream to  the other clients
-        private ObjectOutputStream sendStream;
-        // to get the stream from the other clients
-        private ObjectInputStream getStream;
-        // establishing a server
-        private ServerSocket CentralServer;
-        // setting up the connection between computers
-        private Socket connection;
+    
+    // Here we will add all the required components of the chat application
+    private JFrame frame = new JFrame("Project 4 - Chat Application");
+    private JTextArea textArea = new JTextArea();
+    private JLabel online = new JLabel("Currently Online");
+    private JTextArea clientDisplay = new JTextArea();
+    private JTextField textField = new JTextField(30);
+    private JButton sendButton = new JButton("Send");
+    private JButton connectButton = new JButton("Connect");
+    // to send the stream to  the other clients
+    private ObjectOutputStream sendStream;
+    // to get the stream from the other clients
+    private ObjectInputStream getStream;
+    // establishing a server
+    private ServerSocket CentralServer;
+    // setting up the connection between computers
+    private Socket connection;
+    Boolean connected;
+    // constructor for the chatApp
+    public CentralServer() {
         
-        // constructor for the chatApp
-        public CentralServer() {
-        		
-        		//FRAME FOR THE DISPLAY
-                frame.setSize(600, 375);
-                frame.setLocationRelativeTo(null);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                // creates a new panel to add all the fields in the jframes
-                Panel p1 = new Panel();
-               
-                // Adds the send button with the action listener here
-                sendButton.addActionListener(this);
-                // adds the text field to view the messages exchanged
-                textArea.setEditable(false);
-                textField.setEditable(false);
-                JScrollPane areaScrollPane = new JScrollPane(textArea);
-                areaScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-                areaScrollPane.setPreferredSize(new Dimension(430, 275));
-                
-                //adds the text to view all the clients(current online users)
-                clientDisplay.setEditable(false);
-                JScrollPane areaScrollPane1 = new JScrollPane(clientDisplay);
-                areaScrollPane1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-                areaScrollPane1.setPreferredSize(new Dimension(120, 200));
-                online.setPreferredSize(new Dimension(120,40));
-                // Adds all the fields in the panel
-                p1.add(areaScrollPane);
-                //add a label to the client display
-                //p1.add(online);
-                p1.add(areaScrollPane1);
-                p1.add(textField);
-                p1.add(sendButton);
-                // adds the panel to the frame
-                frame.add(p1);
-                frame.setVisible(true);
+        //FRAME FOR THE DISPLAY
+        frame.setSize(600, 375);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // creates a new panel to add all the fields in the jframes
+        Panel p1 = new Panel();
+        
+        connected = false;
+        // Adds the send button with the action listener here
+        sendButton.addActionListener(this);
+        // Adds the connect button with the action listener here
+        connectButton.addActionListener(this);
+        // adds the text field to view the messages exchanged
+        textArea.setEditable(false);
+        textField.setEditable(false);
+        JScrollPane areaScrollPane = new JScrollPane(textArea);
+        areaScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        areaScrollPane.setPreferredSize(new Dimension(430, 275));
+        
+        //adds the text to view all the clients(current online users)
+        clientDisplay.setEditable(false);
+        JScrollPane areaScrollPane1 = new JScrollPane(clientDisplay);
+        areaScrollPane1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        areaScrollPane1.setPreferredSize(new Dimension(120, 200));
+        online.setPreferredSize(new Dimension(120,40));
+        // Adds all the fields in the panel
+        p1.add(areaScrollPane);
+        //add a label to the client display
+        //p1.add(online);
+        p1.add(areaScrollPane1);
+        p1.add(textField);
+        p1.add(sendButton);
+        p1.add(connectButton);
+        // adds the panel to the frame
+        frame.add(p1);
+        frame.setVisible(true);
+    }
+    
+    //Once the GUI is set we now will establish our connection
+    // Class : RunServer -
+    public void RunServer(){
+        
+        try{
+            // 6789 - port number , where we want to connect
+            // 100 - backlog , number of people can wait
+            CentralServer = new ServerSocket(6789,100);
+            while(true){
+                try{
+                    // here we will connect and have a chat
+                    System.out.println("Waiting for Connection");
+                    waitForConnection(); // To-do :waits for a user to connect
+                    setupStream(); // To-do :initializing the streaming
+                    chatting(); // to-do : while we are chatting
+                }catch(EOFException c){
+                    // this will be encountered when the stream ends
+                    showMessage("\n Connection End !\n ");
+                }finally{ // make sure we close the socket once we are done
+                    closeApp();//closeCrap();
+                }
+            }
+        }catch(IOException c){
+            c.printStackTrace();
         }
         
-        //Once the GUI is set we now will establish our connection
-        // Class : RunServer -
-        public void RunServer(){
-        	
-        	try{
-        		// 6789 - port number , where we want to connect
-        		// 100 - backlog , number of people can wait
-        		CentralServer = new ServerSocket(6789,100);
-        		while(true){
-        			try{
-        				// here we will connect and have a chat
-        				waitForConnection(); // To-do :waits for a user to connect
-        				setupStream(); // To-do :initializing the streaming
-        				chatting(); // to-do : while we are chatting
-        			}catch(EOFException c){
-        				// this will be encountered when the stream ends
-        				showMessage("\n Connection End !\n ");
-        			}finally{ // make sure we close the socket once we are done
-        				closeApp();//closeCrap();
-        			}
-        		}
-        	}catch(IOException c){
-        		c.printStackTrace();
-        	}
-        	
+    }
+    
+    // Method : waitforconnection - this method will wait for the connection to be
+    //            established between the clients and the server
+    private void waitForConnection() throws IOException{
+        showMessage("Current Status : Waiting for a valid connection!\n");
+        connection = CentralServer.accept();
+        showMessage("Current Status : Connected to " + connection.getInetAddress().getHostName());
+    }
+    
+    // Method : setupStream : set up the Stream with the connection found
+    private void setupStream() throws IOException{
+        // setup the output stream
+        sendStream = new ObjectOutputStream(connection.getOutputStream());
+        sendStream.flush();
+        getStream = new ObjectInputStream(connection.getInputStream());
+        showMessage("Current Status : Stream Setup Complete \n");
+    }
+    // Method : chatting - the method in which the actual chatting takes
+    //      place
+    private void chatting() throws IOException{
+        String message = "Connected...." ;
+        sendMessage(message);
+        canType(true);
+        // continue a conversation until the client wants to
+        do{
+            try{
+                message = (String)getStream.readObject();
+                showMessage("\n" +message);
+            }catch(ClassNotFoundException r){
+                showMessage("\n Not a valid object");
+            }
+        }while(!message.equals("END"));
+    }
+    
+    
+    //Method : closeApp - Close the application(streams and socket) when done with it
+    private void closeApp(){
+        showMessage("\n Current Status :Closing the connections \n");
+        canType(false);
+        try{
+            sendStream.close();
+            getStream.close();
+            connection.close();
+            
+        }catch(IOException f){
+            f.printStackTrace();
         }
-        
-        // Method : waitforconnection - this method will wait for the connection to be 
-        //            established between the clients and the server
-        private void waitForConnection() throws IOException{
-        	showMessage("Current Status : Waiting for a valid connection!\n");
-        	connection = CentralServer.accept();
-        	showMessage("Current Status : Connected to " + connection.getInetAddress().getHostName());
-        }
-        
-     // Method : setupStream : set up the Stream with the connection found
-        private void setupStream() throws IOException{
-        	// setup the output stream
-        	sendStream = new ObjectOutputStream(connection.getOutputStream());
+    }
+    
+    // Method : sendMessage : sends the message to the client
+    private void sendMessage(String w) throws IOException{
+        try{
+            sendStream.writeObject("SERVER - " + w);
             sendStream.flush();
-            getStream = new ObjectInputStream(connection.getInputStream());
-            showMessage("Current Status : Stream Setup Complete \n");
+            showMessage("\n SERVER - " + w);
+        }catch(IOException z){
+            textArea.append("\n Message not send\n");
         }
-     // Method : chatting - the method in which the actual chatting takes 
-        //      place
-        private void chatting() throws IOException{
-        	String message = "Connected...." ;
-        	sendMessage(message);
-        	canType(true);
-        	// continue a conversation until the client wants to
-        	do{
-        		try{
-        			message = (String)getStream.readObject();
-        			showMessage("\n" +message);
-        		}catch(ClassNotFoundException r){
-        			showMessage("\n Not a valid object");
-        		}
-        	}while(!message.equals("END"));
-        }
-        
-        
-        //Method : closeApp - Close the application(streams and socket) when done with it
-        private void closeApp(){
-        	showMessage("\n Current Status :Closing the connections \n");
-        	canType(false);
-        	try{
-        		sendStream.close();
-        		getStream.close();
-        		connection.close();
-        		
-        	}catch(IOException f){
-        		f.printStackTrace();
-        	}
-        }
-        
-        // Method : sendMessage : sends the message to the client
-        private void sendMessage(String w) throws IOException{
-        	try{
-        		sendStream.writeObject("SERVER - " + w);
-        		sendStream.flush();
-        		showMessage("\n SERVER - " + w);
-        	}catch(IOException z){
-        		textArea.append("\n Message not send\n");
-        	}
-        }
-        
-        // Method : showMessage: shows the message on the text feild
-        private void showMessage(final String text){
-        	SwingUtilities.invokeLater(
-        			new Runnable(){
+    }
+    
+    // Method : showMessage: shows the message on the text feild
+    private void showMessage(final String text){
+        SwingUtilities.invokeLater(
+                                   new Runnable(){
         				public void run(){
-        					textArea.append(text);
-        				}
-        			}
-        	);
+                            textArea.append(text);
+                        }
         }
-        
-        // Method canType : allows the user to type
-        private void canType(final boolean tof){
-        	SwingUtilities.invokeLater(
-        			new Runnable(){
+                                   );
+    }
+    
+    // Method canType : allows the user to type
+    private void canType(final boolean tof){
+        SwingUtilities.invokeLater(
+                                   new Runnable(){
         				public void run(){
-        					textField.setEditable(tof);
-        				}
-        			}
-        	);
-        	
+                            textField.setEditable(tof);
+                        }
         }
+                                   );
         
-        @Override
-        // HERE WE ONLY HAVE ONE ACTION LISTNER WHICH WILL BE LOCATED ON THE SEND
-        //BUTTON WHUCH READ THE MESSAGE FROM THE TEXT BOX AND DISPLAYS ON THE SCREEN
-        public void actionPerformed(ActionEvent arg0) {
-                String message = textField.getText();
-                textArea.append("UserName: "+ message + "\n");
-                textField.setText("");
+    }
+    
+    // HERE WE ONLY HAVE ONE ACTION LISTNER WHICH WILL BE LOCATED ON THE SEND
+    //BUTTON WHUCH READ THE MESSAGE FROM THE TEXT BOX AND DISPLAYS ON THE SCREEN
+    @Override public void actionPerformed(ActionEvent arg0) {
+        String message = textField.getText();
+        textArea.append("UserName: "+ message + "\n");
+        textField.setText("");
+        if(arg0.getSource() == sendButton)
+        {
+            System.out.println("clicked on send button.");
+            doSendMessage(message);
         }
- 
+        if(arg0.getSource() == connectButton)
+        {
+            System.out.println("clicked on connect button.");
+            doManageConnection();
+        }
+    }
+    
+    public void doSendMessage(String msg)
+    {
+        try{
+            sendStream.writeObject(msg);
+            sendStream.flush();
+            System.out.println(msg);
+        }
+        catch(IOException e){System.out.println(e);}
+    }
+    
+    public void doManageConnection()
+    {
+        if(connected == false)
+        {
+            try{
+                Socket echoSocket = new Socket("192.168.1.67", 6789 );
+                System.out.println(echoSocket);
+                sendStream = new ObjectOutputStream(echoSocket.getOutputStream());
+                sendStream.flush();
+                
+                getStream = new ObjectInputStream(echoSocket.getInputStream());
+                textField.setEditable(true);
+                connected = true;
+                System.out.println("connected is true in doManageConnection.");
+            }
+            catch(IOException e)
+            {
+                System.out.println(e);
+            }
+        }
+        else
+        {
+            try{
+                getStream.close();
+                sendStream.close();
+            }
+            catch(IOException e){System.out.println(e);}
+        }
+    }
+    
 }
