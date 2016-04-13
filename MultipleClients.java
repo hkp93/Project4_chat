ae5e53f83531c8575b0ebfcaf5f506c112138c80
jankipatel07 <janipatel07@gmail.com>
2016-04-13 12:59:40 -0500

    added get methods

--- MultipleClients.java
+++ MultipleClients.java
@@ -1,20 +1,39 @@
 import java.net.InetAddress;
 
-public class MultipleClients {
-
-	public String name;
-	public InetAddress addre;
-	public int port;
-	private final int ID;
-	public int attempt =0;
-	
-	public MultipleClients(String name , InetAddress address , int port,final int ID){
-		this.name = name;
-		this.addre = address;
-		this.port = port;
-		this.ID =ID;
-	}
-	public int getID(){
-		return ID;
-	}
+public class MultipleClients{
+    
+    public String name;
+    public InetAddress addr;
+    public static int port;
+    protected int ID;
+    public int attempt =0;
+    
+    public MultipleClients(){}
+    
+    public MultipleClients(String name , InetAddress address , int port, int ID){
+        this.name = name;
+        this.addr = address;
+        this.port = port;
+        this.ID =ID;
+        
+        System.out.println(name + "  " + address + "  " + port + "  " + ID);
+    }
+    
+    public int getID(){
+        return ID;
+    }
+    public int getPort()
+    {
+        return port;
+    }
+    
+    public InetAddress getIp()
+    {
+        return addr;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
 }
